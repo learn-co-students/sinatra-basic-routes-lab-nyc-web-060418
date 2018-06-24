86175ba32e7818ab3d25552cc2ccd836c0c9e890
Leann Kim <leannminakim@gmail.com>
2018-06-24 16:53:08 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Leann"
+  end
+
+  get '/hometown' do
+    "My hometown is OKC"
+  end
+
+  get '/favorite-song' do
+  # favorite_song = gets.chomp
+  "My favorite song is Cross My Mind"
+  end
 end
