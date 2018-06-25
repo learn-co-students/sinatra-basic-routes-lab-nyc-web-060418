d69fce25100eda45013de7a703c93e45f02a7564
dzeit <dzeit216@gmail.com>
2018-06-25 17:00:45 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is David"
+  end
+
+  get '/hometown' do
+    "My hometown is Efrat, Israel"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is November Rain"
+  end
+
 end
